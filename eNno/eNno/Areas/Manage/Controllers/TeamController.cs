8c2921cd6e8136b8cff1c523ef0842f63e287e0e
[1937forza] <[kadirofff5@outlook.com]>
2022-01-27 23:18:06 +0400

    sasasa

---
+++ eNno/eNno/Areas/Manage/Controllers/TeamController.cs
@@ -0,0 +1,41 @@
+﻿using eNno.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eNno.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class TeamController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TeamController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Teams.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Team team)
+        {
+            if (!ModelState.IsValid) return View();
+            _context.Teams.Add(team);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ eNno/eNno/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using eNno.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eNno.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Team> Teams { get; set; }
+    }
+}
